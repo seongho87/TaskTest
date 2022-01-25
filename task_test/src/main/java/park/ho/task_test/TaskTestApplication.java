e37492cec4fddb9173a2907047422008ca012931
seongho <seongho.park@goodmit.co.kr>
2022-01-25 11:30:43 +0900

    commit test01

---
+++ task_test/src/main/java/park/ho/task_test/TaskTestApplication.java
@@ -0,0 +1,13 @@
+package park.ho.task_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTestApplication.class, args);
+    }
+
+}
